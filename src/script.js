3446a998c195b145586892305d79ef12a40e7167
Lexbree <lexus.olvera@gmail.com>
2022-06-12 16:12:56 -0400

    Corrected brackets to allow function to run

--- src/script.js
+++ src/script.js
@@ -8,8 +8,9 @@ let hours = now.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-
-  let minutes = now.getMinutes();
+}
+let minutes = now.getMinutes();
+{
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
